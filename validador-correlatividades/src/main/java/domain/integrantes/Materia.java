8802a19f72ec452cbe02aad55fc6936d7f6308ee
falpuy <falpuy@frba.utn.edu.ar>
2025-03-31 19:27:35 -0300

    Materia con constructores, getters y setters

--- validador-correlatividades/src/main/java/domain/integrantes/Materia.java
+++ validador-correlatividades/src/main/java/domain/integrantes/Materia.java
@@ -6,4 +6,25 @@ public class Materia {
     private List<Materia> materiasCorrelativas;
     private Integer calificacion;
     private String nombreMateria;
+
+    public Materia(String nombreMateria, List<Materia> materiasCorrelativas) {
+        this.nombreMateria = nombreMateria;
+        this.materiasCorrelativas = materiasCorrelativas;
+    }
+
+    public void setCalificacion(Integer calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getNombreMateria() {
+        return nombreMateria;
+    }
+
+    public List<Materia> getMateriasCorrelativas() {
+        return materiasCorrelativas;
+    }
+
+    public Integer getCalificacion() {
+        return calificacion;
+    }
 }
