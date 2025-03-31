1f1b186a7abff613c023fa4a842c091a3ecbaa05
falpuy <falpuy@frba.utn.edu.ar>
2025-03-31 19:05:26 -0300

    Agregado Inscripcion

---
+++ validador-correlatividades/src/main/java/domain/integrantes/Inscripcion.java
@@ -0,0 +1,12 @@
+package domain.integrantes;
+
+import java.util.List;
+
+public class Inscripcion {
+    private List<Materia> materiasElegidas;
+    private Alumno alumnoSolicitante;
+    public boolean aprobada(Alumno postulante) {
+        //TODO logica de aprobacion de inscripcion
+        return true;
+    }
+}
