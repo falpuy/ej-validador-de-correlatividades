7ef4b15548f9f6b5c6b74d080dac4efa270ed505
falpuy <falpuy@frba.utn.edu.ar>
2025-03-31 19:20:25 -0300

    Alumno con constructores, getters y setters

--- validador-correlatividades/src/main/java/domain/integrantes/Alumno.java
+++ validador-correlatividades/src/main/java/domain/integrantes/Alumno.java
@@ -1,4 +1,48 @@
 package domain.integrantes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumno {
+    private List<Materia> materiasCursadas;
+    private List<Materia> materiasAprobadas;
+    private String nombreAlumno;
+    private String apellidoAlumno;
+    private Integer legajo;
+
+    public Alumno(String nombreAlumno, String apellidoAlumno, Integer legajo) {
+        this.nombreAlumno = nombreAlumno;
+        this.apellidoAlumno = apellidoAlumno;
+        this.legajo = legajo;
+        this.materiasCursadas = new ArrayList<>();
+        this.materiasAprobadas = new ArrayList<>();
+    }
+
+    public List<Materia> getMateriasCursadas() {
+        return materiasCursadas;
+    }
+
+    public void setMateriasCursadas(List<Materia> materiasCursadas) {
+        this.materiasCursadas = materiasCursadas;
+    }
+
+    public List<Materia> getMateriasAprobadas() {
+        return materiasAprobadas;
+    }
+
+    public void setMateriasAprobadas(List<Materia> materiasAprobadas) {
+        this.materiasAprobadas = materiasAprobadas;
+    }
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public String getApellidoAlumno() {
+        return apellidoAlumno;
+    }
+
+    public Integer getLegajo() {
+        return legajo;
+    }
 }
--- validador-correlatividades/src/main/java/domain/integrantes/Materia.java
+++ validador-correlatividades/src/main/java/domain/integrantes/Materia.java
@@ -1,4 +1,9 @@
 package domain.integrantes;
 
+import java.util.List;
+
 public class Materia {
+    private List<Materia> materiasCorrelativas;
+    private Integer calificacion;
+    private String nombreMateria;
 }
