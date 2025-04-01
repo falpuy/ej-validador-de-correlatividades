5975f431fb9cbaf5a02778a05b4c5387b26b26d4
falpuy <falpuy@frba.utn.edu.ar>
2025-03-31 22:06:57 -0300

    Inscripcion con metodo aprobada

--- validador-correlatividades/src/main/java/domain/integrantes/Inscripcion.java
+++ validador-correlatividades/src/main/java/domain/integrantes/Inscripcion.java
@@ -1,12 +1,17 @@
 package domain.integrantes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Inscripcion {
     private List<Materia> materiasElegidas;
     private Alumno alumnoSolicitante;
-    public boolean aprobada(Alumno postulante) {
-        //TODO logica de aprobacion de inscripcion
-        return true;
+    public boolean aprobada() {
+        List<Boolean> aceptacionesYfallos = new ArrayList<>();
+        this.materiasElegidas.forEach( (asignatura) -> {
+            boolean resultado = this.alumnoSolicitante.getMateriasAprobadas().containsAll(asignatura.getMateriasCorrelativas());
+            aceptacionesYfallos.add(resultado);
+        } );
+        return !aceptacionesYfallos.contains(false);
     }
 }
